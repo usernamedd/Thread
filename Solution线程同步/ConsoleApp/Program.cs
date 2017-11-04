6446faaf152f8ccd46249759e57604148ca4674b
ddx1292680719 <1292680719@qq.com>
2017-11-04 08:57:17 +0800

    添加ConsoleApp项目；修改.gitignore，不跟踪bin和obj文件夹；

---
+++ Solution线程同步/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(new ThreadStart(ThreadMethod)).Start();
+            Console.ReadLine();
+        }
+
+        static void ThreadMethod()
+        {
+            Console.WriteLine("托管线程ID:{0}",Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
